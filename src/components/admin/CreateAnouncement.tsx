d2fc62292f2b29bec6af1b81f13db24294fe6cf0
lavvillaroza <lance.villaroza017@gmail.com>
2024-11-26 00:05:09 +0800

    UpdatingSourceCode for Portfolio 3

--- src/components/admin/CreateAnouncement.tsx
+++ src/components/admin/CreateAnouncement.tsx
@@ -49,13 +49,19 @@ const CreateAnouncement: React.FC = () => {
         e.preventDefault();
     
         const user = localStorage.getItem('userDetails');
-        // Combine date with start and end times
-        const dateTimeFrom = new Date(
-            `${formattedDate?.toLocaleDateString()} ${startTime}`
-        );
-        const dateTimeTo = new Date(
-            `${formattedDate?.toLocaleDateString()} ${endTime}`
-        );
+        const [year, month, day] = [
+            formattedDate?.getFullYear() ?? 1970, // Default year if undefined
+            formattedDate?.getMonth() ?? 0,      // Default month (January) if undefined
+            formattedDate?.getDate() ?? 1,       // Default day if undefined
+        ];
+        
+
+        const [fromHours, fromMinutes] = startTime.split(':').map(Number);
+        const [toHours, toMinutes] = endTime.split(':').map(Number);
+
+        // Combine date with start and end times        
+        const dateTimeFrom = new Date(year, month, day, fromHours ?? 0, fromMinutes ?? 0);
+        const dateTimeTo = new Date(year, month, day, toHours ?? 0, toMinutes ?? 0);
     
         const formData = new FormData();
         formData.append('id', '0');
@@ -288,7 +294,7 @@ const CreateAnouncement: React.FC = () => {
                                                         required/>
                                                     <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-s-0 border-s-0 border-gray-300 rounded-e-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
                                                         <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                                            <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z" clip-rule="evenodd"/>
+                                                            <path fillRule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z" clipRule="evenodd"/>
                                                         </svg>
                                                     </span>
                                                 </div>                                               
@@ -302,7 +308,7 @@ const CreateAnouncement: React.FC = () => {
                                                         required/>
                                                     <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-s-0 border-s-0 border-gray-300 rounded-e-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
                                                         <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                                            <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z" clip-rule="evenodd"/>
+                                                            <path fillRule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z" clipRule="evenodd"/>
                                                         </svg>
                                                     </span>
                                                 </div>                                               
